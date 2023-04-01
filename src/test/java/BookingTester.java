970e24591c69752ccc0e8698e0a5896a21c99802
hoangvbm2808 <87510794+hoangvbm2808@users.noreply.github.com>
2023-04-01 15:52:57 +0700

    Create BookingTester.java
    Create BookingTester and add unit test checkSDT and test add Ticket

---
+++ src/test/java/BookingTester.java
@@ -0,0 +1,96 @@
+
+import com.myproject.conf.jdbcUtils;
+import com.myproject.pojo.Account;
+import com.myproject.pojo.VeXe;
+import com.myproject.services.AccountService;
+import com.myproject.services.BookingService;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class BookingTester {
+    private static final BookingService bk = new BookingService();
+    private static Connection conn;
+    
+    @BeforeAll
+    public static void openConn() {
+        try {
+            conn = jdbcUtils.getConn();
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+    
+    @AfterAll
+    public static void closeConn() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(AccountTester.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
+    @Test
+    public void testCheckSDT() throws SQLException{
+        boolean actual = bk.checkSDT("0399987202");
+        Assertions.assertTrue(actual);
+    }
+    
+    @Test
+    public void testCheckSDT2() throws SQLException{
+        boolean actual = bk.checkSDT("5399987202");
+        Assertions.assertFalse(actual);
+    }
+    
+    @Test
+    public void testCheckSDT3() throws SQLException{
+        boolean actual = bk.checkSDT("987202");
+        Assertions.assertFalse(actual);
+    }
+    
+    @Test
+    public void testAddTicket() throws SQLException{
+        VeXe ve = new VeXe("3","Minh Hoang", Date.valueOf(LocalDate.now()) , "0399987202", 
+            7, "B02", "Đã đặt", 
+            2, 1, "Nha Trang");
+       
+        try {
+            bk.addVeXe(ve);
+            String MaChuyen = ve.getMaVe();
+//            Assertions.assertTrue(actual);
+            
+            PreparedStatement stm = conn.prepareCall("SELECT * FROM vexe WHERE id=?");
+            stm.setString(1, ve.getMaVe());
+            
+            ResultSet rs = stm.executeQuery();
+            Assertions.assertNotNull(rs.next());
+            Assertions.assertEquals(MaChuyen, rs.getString("id"));
+            Assertions.assertEquals("Minh Hoang", rs.getString("tenKhachHang"));
+        } catch (SQLException ex) {
+            Logger.getLogger(BookingTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//        Assertions.assertFalse(actual);
+    }
+}
